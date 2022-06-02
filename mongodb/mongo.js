8c9727b5e4bc4d52184f6b0dc39caf70d616ce16
Jatin Vishal Mishra <81311135+jatinmishra517@users.noreply.github.com>
2022-06-02 21:54:30 +0530

    Create mongo.js

---
+++ mongodb/mongo.js
@@ -0,0 +1,27 @@
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/chattydb";
+//exemplar syntax for insertion creation and deletion
+
+//create a collection 
+
+// MongoClient.connect(url, function(err, db) {
+//     if (err) throw err;
+//     var dbo = db.db("mydb");
+//     dbo.createCollection("customers", function(err, res) {
+//       if (err) throw err;
+//       console.log("Collection created!");
+//       db.close();
+//     });
+//   });
+
+// insert a document
+MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("mydb");
+    var myobj = { name: "Company Inc", address: "Highway 37" };
+    dbo.collection("customers").insertOne(myobj, function(err, res) {
+      if (err) throw err;
+      console.log("1 document inserted");
+      db.close();
+    });
+  });
